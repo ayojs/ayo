8e049c1391ca829a33c3f34d0704c930b2a226e9
Anna Henningsen <anna@addaleax.net>
2017-09-06 23:20:07 +0200

    test: add basic tests for MessageChannel
    PR-URL: https://github.com/ayojs/ayo/pull/116
    Reviewed-By: Benjamin Gruenbaum <benjamingr@gmail.com>
    Reviewed-By: Stephen Belanger <admin@stephenbelanger.com>

---
+++ test/parallel/test-message-channel-sharedarraybuffer.js
@@ -0,0 +1,28 @@
+/*global SharedArrayBuffer*/
+'use strict';
+// Flags: --harmony-sharedarraybuffer --expose-gc
+
+const common = require('../common');
+const assert = require('assert');
+const { Worker } = require('worker');
+
+{
+  const sharedArrayBuffer = new SharedArrayBuffer(12);
+  const local = Buffer.from(sharedArrayBuffer);
+
+  const w = new Worker(`
+    require('worker').on('workerMessage', ({ sharedArrayBuffer }) => {
+      const local = Buffer.from(sharedArrayBuffer);
+      local.write('world!', 6);
+      require('worker').postMessage();
+    });
+  `, { eval: true });
+  w.on('message', common.mustCall(() => {
+    assert.strictEqual(local.toString(), 'Hello world!');
+    global.gc();
+    w.terminate();
+  }));
+  w.postMessage({ sharedArrayBuffer });
+  // This would be a race condition if the memory regions were overlapping
+  local.write('Hello ');
+}
