26590a244513d7debfb1c362105f638108b0f9be
Timothy Gu <timothygu99@gmail.com>
2017-09-19 22:12:30 -0700

    events: move EventEmitter to V8 Extras
    PR-URL: https://github.com/ayojs/ayo/pull/122
    Reviewed-By: Stephen Belanger <admin@stephenbelanger.com>
    Reviewed-By: Anna Henningsen <anna@addaleax.net>

---
+++ src/node_extras.cc
@@ -0,0 +1,30 @@
+#include "node_internals.h"
+#include "util.h"
+#include "util-inl.h"
+#include "v8.h"
+
+namespace node {
+
+using v8::Context;
+using v8::Isolate;
+using v8::Local;
+using v8::Object;
+using v8::Value;
+
+namespace v8_extras {
+
+static void Init(Local<Object> target,
+                 Local<Value> unused,
+                 Local<Context> context,
+                 void* priv) {
+  Isolate* isolate = context->GetIsolate();
+  Local<Object> binding = context->GetExtrasBindingObject();
+  CHECK(target->Set(context, FIXED_ONE_BYTE_STRING(isolate, "binding"),
+                    binding).FromMaybe(false));
+}
+
+}  // namespace v8_extras
+
+}  // namespace node
+
+NODE_MODULE_CONTEXT_AWARE_INTERNAL(extras, node::v8_extras::Init)
