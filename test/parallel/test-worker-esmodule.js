7711dee926a756b7dacedd02c5cc598fc3158f1e
Anna Henningsen <anna@addaleax.net>
2017-09-22 17:17:57 +0200

    test: add workers + ES Module test

---
+++ test/parallel/test-worker-esmodule.js
@@ -0,0 +1,11 @@
+'use strict';
+// Flags: --experimental-modules
+const common = require('../common');
+const fixtures = require('../common/fixtures');
+const assert = require('assert');
+const { Worker } = require('worker');
+
+const w = new Worker(fixtures.path('worker-script.mjs'));
+w.on('message', common.mustCall((message) => {
+  assert.strictEqual(message, 'Hello, world!');
+}));
