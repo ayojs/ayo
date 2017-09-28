55356eb5c1d0435c062452aeab5904207c4b0156
Anna Henningsen <anna@addaleax.net>
2017-09-28 19:15:08 +0200

    [squash] add explanatory comment

--- src/req-wrap-inl.h
+++ src/req-wrap-inl.h
@@ -126,6 +126,21 @@ template <typename T>
 template <typename LibuvFunction, typename... Args>
 int ReqWrap<T>::Dispatch(LibuvFunction fn, Args... args) {
   Dispatched();
+
+  // This expands as:
+  //
+  // return fn(env()->event_loop(), req(), arg1, arg2, Wrapper, arg3, ...)
+  //           ^                                       ^        ^
+  //           |                                       |        |
+  //           \-- Omitted if `fn` has no              |        |
+  //               first `uv_loop_t*` argument         |        |
+  //                                                   |        |
+  //     A function callback whose first argument      |        |
+  //     matches the libuv request type is replaced ---/        |
+  //     by the `Wrapper` method defined above                  |
+  //                                                            |
+  //            Other (non-function) arguments are passed  -----/
+  //            through verbatim
   return CallLibuvFunction<T, LibuvFunction>::Call(
       fn,
       env()->event_loop(),
